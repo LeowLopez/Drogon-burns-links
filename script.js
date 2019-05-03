afcbc611d016f5500fc50ed38f06561fb92710fe
Leo Wender <leowlopez@live.com>
2019-05-03 19:41:22 -0300

    Initial commit
    :)

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+	dracarys = false;
+	dracarysButton = document.getElementById("Drogon");
+	dracarysButton.addEventListener("click", DracarysOrNot);
+});
+
+function DracarysOrNot(){
+
+	if(dracarys == false){
+		links = document.queryselectorAll("a");
+		linkNumbers = links.length;
+		Oldlinks = new Array(linkNumbers);//to save the old class
+		
+		for(i = 0; i < linkNumbers; i++){
+			Oldlinks[i] = links[i].className;//saving
+			links[i].className = "Burning";//renaming class
+			console.log(i);
+		}
+		dracarysButton.value = "Recuperar/Recover";
+		dracarys = true;
+		console.log(dracarys);
+	}
+
+	else{//recover the class
+
+		for(i = 0; i < linkNumbers; i++){
+			links[i].className = Oldlinks[i];//renaming class
+			console.log(i);
+		}
+		dracarysButton.value = "Dracarys!";
+		dracarys = false;
+		console.log(dracarys);
+	}
+}

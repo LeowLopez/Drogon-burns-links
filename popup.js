0331aa051d3c78084c72230d8c7b4d29bc0dc3e2
Leo Wender <leowlopez@live.com>
2019-05-08 18:08:42 -0300

    storage settings added
    Now when the user clicks on the button, and close popup, on click again the name it's the same.
    Bugs:
    1. if reload the page, the button don't reload
    2. if change tab, button don't reload (maybe transforming vars in arrays by tab active, solve it)
    Beste solution: Change code for every time at the user open the popup, its send a message to content.js and verify the burning class existence, if more than 1, button is "Recover", else "Dracarys".

--- popup.js
+++ popup.js
@@ -1,4 +1,20 @@
-buttonStats = true;
+function renameButton(when){
+
+	chrome.storage.sync.get("status", function(buttonName){//get button name on storage
+		
+		if(when == "clickPopup"){//Every click on popup extension
+			if(buttonName.status){//had saved some status (if true)
+				buttonStatus = buttonName.status;//recover status (button name)
+			} else buttonStatus = "dracarys";//First click on popup extension (nothing saved)	
+		}
+		
+		if(buttonStatus == "recover"){
+			buttonDracarys.innerHTML = "Recuperar";
+		} else buttonDracarys.innerHTML = "Dracarys!";
+
+		chrome.storage.sync.set({"status": buttonStatus});//save button name on storage
+	})	
+}
 
 function popup() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
@@ -6,17 +22,15 @@ function popup() {
     chrome.tabs.sendMessage(activeTab.id, {"message": "dracarys"});//send to content.js (injected on actual page)
     });
 
-	if(buttonStats == true){
-		buttonDracarys.innerHTML = "Recuperar";
-		buttonStats = false;
-	}
-	else{
-		buttonDracarys.innerHTML = "Dracarys!";
-		buttonStats = true;
-	}
+	if(buttonStatus == "recover"){
+		buttonStatus = "dracarys";
+	} else buttonStatus = "recover";
+	
+    renameButton();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
 	buttonDracarys = document.getElementById("Drogon");
+	renameButton("clickPopup");
 	buttonDracarys.addEventListener("click", popup);
 });

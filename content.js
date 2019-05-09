a19ed2792db6f843f38317fee507886252904e8e
Leo Wender <leowlopez@live.com>
2019-05-09 15:04:58 -0300

    Fix storage/reload/tabs bugs
    Storage removed;
    Added messages communication in all events between popup.js and content.js, to synchronize button name without bugs

--- content.js
+++ content.js
@@ -4,6 +4,16 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.message === "dracarys"){
 		DracarysOrNot();
 	}
+	else if((request.message === "DaenerysIsHere") && (dracarys == false)){
+		console.log(request.message);
+		sendResponse({answer:"imNotYourPrincess"});
+	}
+	else if((request.message === "DaenerysIsHere") && (dracarys == true)){
+		console.log(request.message);
+		sendResponse({answer:"FireAndBlood"});		
+	}else {
+		console.log("Dragon is not a slave");		
+	}
 });
 
 
--- popup.js
+++ popup.js
@@ -1,36 +1,30 @@
-function renameButton(when){
+function renameButton(){
 
-	chrome.storage.sync.get("status", function(buttonName){//get button name on storage
+	chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+		var activeTab = tabs[0];
+	    	chrome.tabs.sendMessage(activeTab.id, {"message": "DaenerysIsHere"}, function(response){//send to content.js (injected on actual page)
+			//console.log(response.answer);
+			buttonAnswer = response.answer;
+				
+			if(buttonAnswer == "FireAndBlood"){
+				buttonDracarys.innerHTML = "Backup";
+			} else buttonDracarys.innerHTML = "Dracarys!";
+		});
+	});
 		
-		if(when == "clickPopup"){//Every click on popup extension
-			if(buttonName.status){//had saved some status (if true)
-				buttonStatus = buttonName.status;//recover status (button name)
-			} else buttonStatus = "dracarys";//First click on popup extension (nothing saved)	
-		}
-		
-		if(buttonStatus == "recover"){
-			buttonDracarys.innerHTML = "Recuperar";
-		} else buttonDracarys.innerHTML = "Dracarys!";
-
-		chrome.storage.sync.set({"status": buttonStatus});//save button name on storage
-	})	
 }
 
 function popup() {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "dracarys"});//send to content.js (injected on actual page)
-    });
-
-	if(buttonStatus == "recover"){
-		buttonStatus = "dracarys";
-	} else buttonStatus = "recover";
+	chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    		var activeTab = tabs[0];
+    		chrome.tabs.sendMessage(activeTab.id, {"message": "dracarys"});
+    	});
 	
-    renameButton();
+   	renameButton();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
 	buttonDracarys = document.getElementById("Drogon");
-	renameButton("clickPopup");
+	renameButton();
 	buttonDracarys.addEventListener("click", popup);
 });
